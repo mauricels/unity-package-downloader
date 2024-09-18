ee955fb7b5fcc608e6fe39730fe2acb161367856
= <maurice.josef@gmail.com>
2024-09-18 23:40:13 +0200

    User Input: Added more inputs for version and custom folder. Also added validation for input.

--- unity_package_downloader/downloader/programm.py
+++ unity_package_downloader/downloader/programm.py
@@ -1,9 +1,54 @@
-def prompt_user_for_package():
-    # Fragt den Benutzer nach dem Paketnamen
-    package = input("Welches Paket möchtest du installieren? (z.B. com.unity.textmeshpro): ")
-    
-    # Fragt den Benutzer nach dem Installationsort
-    location = input("Wo möchtest du das Paket installieren? (cache oder benutzerdefinierter Pfad): ")
-    
-    return package, location
+import os
+import re
+
+YES_RESP = ['Y', 'y', 'yes']
+VERSION_RE = r'^\d+(\.\d+)*$'
+
+
+class UserInputHandler:
+
+    def prompt_user_for_input(self):
+        # asks the user which package he wants to install
+        package = input("Which package do you want to install? Example: com.unity.textmeshpro")
+        
+        # asks the user which version of the package he wants to install
+        version = input('Which version do you want to install? Example: 1.0.1')
+
+        # asks the user if he wants if he wants to install it into a custom folder or use the unity cache 
+        use_unity_cache = input('Do you want to use the default win unity cache folder? (Y)es or (N)o?')
+
+        if use_unity_cache in YES_RESP:
+            return package, version, True, None 
+        
+        # asks the user to enter a custom folder 
+        custom_folder = input('Please enter a custom folder...')
+        
+        return package, version, False, custom_folder
+
+    def check_user_input(self, package, version, use_unity_cache, custom_folder):
+        correct = True 
+
+        if not package.strip():
+            print('The package name is empty')
+            correct = False 
+
+        if not version.strip():
+            print('The version is empty')
+            correct = False 
+
+        if not re.match(VERSION_RE, version):
+            print('The version is invalid')
+            correct = False
+
+        if use_unity_cache in YES_RESP: 
+            return correct 
+
+        if not custom_folder.strip():
+            print('The version is empty')
+            correct = False
+        elif not os.path.exists(custom_folder):
+            print('The custom folder does not exist')
+            correct = False
+
+        return correct 
 
--- unity_package_downloader/tests/test_programm.py
+++ unity_package_downloader/tests/test_programm.py
@@ -1,21 +1,56 @@
 import unittest
+import os
 from unittest.mock import patch
-from downloader.programm import prompt_user_for_package
+from downloader.programm import UserInputHandler 
 
 class TestUserInput(unittest.TestCase):
-    @patch('builtins.input', side_effect=['com.unity.textmeshpro', 'cache'])
-    def test_prompt_user_for_package(self, mock_input):
-        # Testet, ob die Funktion korrekt nach Paketnamen und Installationsort fragt
-        package, location = prompt_user_for_package()
-        self.assertEqual(package, 'com.unity.textmeshpro')
-        self.assertEqual(location, 'cache')
 
-    @patch('builtins.input', side_effect=['com.unity.package', '/custom/path'])
-    def test_prompt_user_for_package_custom_path(self, mock_input):
-        # Testet, ob ein benutzerdefinierter Pfad korrekt abgefragt wird
-        package, location = prompt_user_for_package()
-        self.assertEqual(package, 'com.unity.package')
-        self.assertEqual(location, '/custom/path')
+    @classmethod
+    def setUpClass(cls):
+        cls.handler = UserInputHandler()
+
+
+    # test if the function returns correct user inputs
+    @patch('builtins.input', side_effect=['com.unity.textmeshpro', '1.0.1', 'yes', ''])
+    def test_prompt_user_for_input_correct(self, mock_input):
+        package, version, use_unity_cache, custom_folder = self.handler.prompt_user_for_input()
+        self.assertEqual(package, 'com.unity.textmeshpro')
+        self.assertEqual(version, '1.0.1')
+        self.assertEqual(use_unity_cache, True)
+        self.assertEqual(custom_folder, None)
+
+    # test if the function returns correct user inputs using a custom_folder
+    @patch('builtins.input', side_effect=['com.unity.textmeshpro', '1.0.1', 'no', 'custom_folder'])
+    def test_prompt_user_for_input_correct_custom_folder(self, mock_input):
+        package, version, use_unity_cache, custom_folder = self.handler.prompt_user_for_input()
+        self.assertEqual(package, 'com.unity.textmeshpro')
+        self.assertEqual(version, '1.0.1')
+        self.assertEqual(use_unity_cache, False)
+        self.assertEqual(custom_folder, 'custom_folder')
+
+    def test_valid_input(self):
+        self.assertTrue(self.handler.check_user_input('com.unity.correct', '1.0.1', 'yes', ''))
+
+
+    def test_invalid_input_empty_package_name(self):
+        self.assertFalse(self.handler.check_user_input('', '1.0.1', 'yes', ''))
+
+
+    def test_invalid_input_empty_version(self):
+        self.assertFalse(self.handler.check_user_input('com.unity.sexy', '', 'yes', ''))
+
+
+    @patch('os.path.exists', return_value=True)
+    def test_valid_input_custom_folder(self, mock_exists):
+        self.assertTrue(self.handler.check_user_input('com.unity.correct', '1.0.1', 'no', 'custom_folder'))
+
+    @patch('os.path.exists', return_value=False)
+    def test_invalid_input_custom_folder_not_exists(self, mock_exists):
+        self.assertFalse(self.handler.check_user_input('com.unity.correct', '1.0.1', 'no', 'custom_folder'))
+
+
+    def test_invalid_input_version(self):
+        self.assertFalse(self.handler.check_user_input('com.unity.correct', '1ghsigeoh-0.1', 'yes', ''))
 
 if __name__ == '__main__':
     unittest.main()
