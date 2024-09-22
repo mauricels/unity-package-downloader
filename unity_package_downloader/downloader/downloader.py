5d1e72055ceef731d22defcd21c5d194e265241e
mauricels_wsl_win <maurice.josef@gmail.com>
2024-09-22 23:14:31 +0200

    Downloader: Added method for downloading package from url

--- unity_package_downloader/downloader/downloader.py
+++ unity_package_downloader/downloader/downloader.py
@@ -6,9 +6,24 @@ class UnityPackage:
     def __init__(self, package_name, version):
         self.package_name = package_name
         self.version = version
-        self.unity_url = f'https://packages.unity.com/{self.package_name}'
+        self.combined = self.package_name + '@' + self.version
+        self.current_url = f'https://packages.unity.com/{self.package_name}'
 
     def fetch_package_data(self, url):
         response = requests.get(url)
         response.raise_for_status()  # Raise an error for bad responses
         return response.json()
+
+    def download_tgz(self, url, destination):
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(destination, 'wb') as file:
+                for chunk in response.iter_content(chunksize=128):
+                    file.write(chunk)
+            return destination
+        else:
+            raise Exception(f'Failed to downloade package {self.combined}: {response.status_code}')
+
+    def download_package(self):
+        return self.download_tgz(self.current_url, self.combined)
+
--- unity_package_downloader/tests/test_downloader.py
+++ unity_package_downloader/tests/test_downloader.py
@@ -1,5 +1,6 @@
 import unittest
-from unittest.mock import patch
+import requests
+from unittest.mock import patch, MagicMock
 from downloader.downloader import UnityPackage
 
 # Assuming UnityPackage is implemented elsewhere
@@ -27,13 +28,48 @@ class TestUserInput(unittest.TestCase):
         mock_get.return_value.json.return_value = mock_response
 
         # Assuming UnityPackage is implemented elsewhere with fetch_package_data method
-        fetch_result = UnityPackage("com.unity.burst", "1.8.9").fetch_package_data("https://packages.unity.org/just_a_mock")
+        fetch_result = UnityPackage('com.unity.burst', '1.8.9').fetch_package_data('https://packages.unity.org/just_a_mock')
 
         # Verify that the metadata is correct
         self.assertEqual(fetch_result['_id'], 'com.unity.burst')
         self.assertEqual(fetch_result['versions']['1.8.9']['dist']['tarball'],
                          'https://download.packages.unity.com/com.unity.burst/-/com.unity.burst-1.8.9.tgz')
 
+    @patch('requests.get')
+    def test_download_tgz(self, mock_get):
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.iter_content = MagicMock(return_value=[b'chunk1', b'chunk2'])
+        mock_get.return_value = mock_response
+
+
+        package = UnityPackage('com.unity.burst', '1.8.9')
+
+        result = package.download_tgz('https://download.packages.unity.com/com.unity.burst/-/com.unity.burst-1.8.9.tgz', 'com.unity.burst-1.8.9.tgz')
+
+        self.assertEqual(result, 'com.unity.burst-1.8.9.tgz')
+
+        mock_get.assert_called_with('https://download.packages.unity.com/com.unity.burst/-/com.unity.burst-1.8.9.tgz', stream=True)
+
+
+        with open('com.unity.burst-1.8.9.tgz', 'rb') as file:
+            file_content = file.read()
+            self.assertEqual(file_content, b'chunk1chunk2')
+
+    @patch('downloader.downloader.UnityPackage.download_tgz')
+    def test_download_package(self, mock_download_tgz):
+        package_name = 'com.unity.burst'
+        version = '1.8.9'
+        package = UnityPackage(package_name, version)
+
+        package.download_package()
+
+        mock_download_tgz.assert_called_once_with(package.current_url, package.combined)
+         
+
+    def test_get_combinded_package_name(self):
+        package = UnityPackage('hello', '1.0')
+        self.assertEqual(package.combined, 'hello@1.0')
 
 if __name__ == '__main__':
     unittest.main()
